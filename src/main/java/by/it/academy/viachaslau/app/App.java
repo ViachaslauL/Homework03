772cb0d7d34fdcb410d25ac38cbf4acc3691c346
ViachaslauL <Lotussutol@tut.by>
2021-09-28 19:41:52 +0300

    start project

---
+++ src/main/java/by/it/academy/viachaslau/app/App.java
@@ -0,0 +1,13 @@
+package by.it.academy.viachaslau.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
